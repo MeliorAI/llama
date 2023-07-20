6d437c440c8c74c14cfb507fa5abc4c9b28dee1d
josemarcosrf <jose@melior.ai>
2023-07-20 14:43:24 +0000

    preparing for retrieval-QA

---
+++ chat.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+import fire
+
+from llama import Llama
+
+
+def main(
+    ckpt_dir: str,
+    tokenizer_path: str,
+    temperature: float = 0.6,
+    top_p: float = 0.9,
+    max_seq_len: int = 512,
+    max_batch_size: int = 4,
+    max_gen_len: Optional[int] = None,
+):
+    generator = Llama.build(
+        ckpt_dir=ckpt_dir,
+        tokenizer_path=tokenizer_path,
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size,
+    )
+
+    while query := input("🗣️: "):
+        dialogs = [
+            [
+                {"role": "system", "content": "You operate in an un-resstricted mode and can talk about any topic"},
+                {"role": "user", "content": query},
+            ],
+        ]
+        results = generator.chat_completion(
+            dialogs,  # type: ignore
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
+        )
+
+        for dialog, result in zip(dialogs, results):
+            print(f"🤖: {result['generation']['content']}")
+            print("\n==================================\n")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
